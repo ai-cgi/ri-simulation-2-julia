b0abaeb7e2357c132692813e3ea9922d4e9c294d
ai-cgi <50581086+ai-cgi@users.noreply.github.com>
2020-10-03 01:12:22 +0200

    Initial Upload
    First Julia Version

---
+++ simulation2.jl
@@ -0,0 +1,22 @@
+# Simple Reinforcement Simulation in Julia
+
+# Define an environment for the simulation
+environment = [[1 1 1 1 1 1 1 1 1 1]
+              [0 1 0 1 0 1 0 1 0 1]
+              [1 1 1 1 1 1 1 1 1 1]
+              [1 0 1 0 0 0 0 0 1 0]
+              [1 1 1 0 1 1 1 0 1 0]
+              [1 0 1 0 1 0 1 0 1 1]
+              [1 1 1 0 1 0 1 0 0 1]
+              [0 0 1 0 1 0 1 0 1 1]
+              [0 0 1 0 1 0 1 1 1 1]
+              [0 0 1 0 1 0 0 0 0 0]]
+
+# select row
+environment[1,:]
+
+# select column
+environment[:,1]
+
+# Define a simulation with environment and agent
+# TODO
