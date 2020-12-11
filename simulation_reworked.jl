fb2dfe15ddeead0742e7b3167d177ae56aad499b
dade <dade@dades-mbp.speedport.ip>
2020-12-12 00:02:04 +0100

    Changed Rewards

--- simulation_reworked.jl
+++ simulation_reworked.jl
@@ -18,17 +18,13 @@ final_state = State(Offset(10,3))
 initial_state = State(Offset(10,5))
 
 # defining all rewards
-reward_1 = Reward(Offset(5,5), Offset(0,0),3)
-reward_2 = Reward(Offset(5,7), Offset(0,0),3)
-reward_3 = Reward(Offset(9,7), Offset(0,0),3)
-reward_4 = Reward(Offset(9,10), Offset(0,0),3)
-reward_5 = Reward(Offset(6,10), Offset(0,0),3)
-reward_6 = Reward(Offset(3,9), Offset(0,0),3)
-reward_7 = Reward(Offset(3,3), Offset(0,0),3)
-reward_8 = Reward(Offset(10,3), Offset(0,0),100)
+reward_1 = Reward(Offset(9,10), Offset(0,0),3)
+reward_2 = Reward(Offset(3,7), Offset(0,0),3)
+reward_3 = Reward(Offset(4,3), Offset(0,0),3)
+reward_4 = Reward(Offset(10,3), Offset(0,0),100)
 
 # create reward list
-reward_list = [reward_1, reward_2, reward_3, reward_4, reward_5, reward_6, reward_7, reward_8]
+reward_list = [reward_1, reward_2, reward_3, reward_4]
 last_reward = 0
 last_action = "up"
 number_of_steps = 0
@@ -45,7 +41,7 @@ agent_x = Agent(Offset(10,5),0)
 function get_state_for_agent(agent)
          string(agent.pos.x,".",agent.pos.y)
 end
- 
+
 function set_state_for_agent(state)
   Agent(state.pos, 0)
 end
@@ -55,7 +51,7 @@ function state_to_string(state)
 end
 
 function string_to_state(str)
-  State(Offset(parse(Int, split(str, ".")[1]),parse(Int, split(str, ".")[2]))) 
+  State(Offset(parse(Int, split(str, ".")[1]),parse(Int, split(str, ".")[2])))
 end
 
 function get_possible_actions(agent)
@@ -126,11 +122,11 @@ end
 
 "create search tree for searching"
 function create_tree(tree, state_string)
-  if is_in_final_state(set_state_for_agent(string_to_state(state_string))) 
-    tree[state_string] = Dict()
-    tree
-  else
-     if !haskey(tree, state_string)
+  println(state_string)
+  if !haskey(tree, state_string)
+    if is_in_final_state(set_state_for_agent(string_to_state(state_string)))
+      tree[state_string] = Dict()
+    else
       agt = set_state_for_agent(string_to_state(state_string))
       pos_actions = get_possible_actions(agt)
       sub_tree = Dict()
@@ -139,20 +135,19 @@ function create_tree(tree, state_string)
         if !haskey(tree,target)
            sub_tree[act] = target
         end
-           agt = set_state_for_agent(string_to_state(state_string))
+        agt = set_state_for_agent(string_to_state(state_string))
       end
       tree[state_string] = sub_tree
       for s in values(sub_tree)
-        create_tree(tree,s)     
-      end       
-    end 
+        tree = create_tree(tree,s)
+      end
+    end
   end
     tree
 end
 
 "function to visualize trees based on dictionary relations by given start_key"
 function visualize_tree(tree_dict, key, dot_string)
-   println("->", dot_string)
    edges = tree_dict[key]
     if length(edges) == 0
     else
@@ -160,15 +155,20 @@ function visualize_tree(tree_dict, key, dot_string)
            edge = edges[ke]
            dot_string = string(dot_string, key, " -> ", edge, " [label=",first(ke),"]\n")
            dot_string = visualize_tree(tree_dict, edge, dot_string)
-      end     
+      end
    end
-  dot_string 
-end   
+  dot_string
+end
 
-graphviz_start = """digraph {\n    
+"simulates the monte carlo walk thru the tree to the target"
+function mc_walk_tree_to_target(tree)
+
+end
+
+graphviz_start = """digraph {\n
       node [shape="circle", style="filled", fillColor="#AAAAAA"]\n
-      edge [color="#AAAAAA", fontcolor="#AAAAAA"]\n"""    
-  
+      edge [color="#AAAAAA", fontcolor="#AAAAAA"]\n"""
+
 function highlist_start_stop(graphviz_str, start_state, end_state)
   graphviz_str = string(graphviz_str, """ $start_state [color="darkgreen", fontcolor="white"]\n""")
   graphviz_str = string(graphviz_str, """ $end_state [color="#AA0000", fontcolor="white"]\n""")
@@ -180,4 +180,3 @@ function add_rewards_to_graphviz_str(graphviz_str)
   end
   graphviz_str
 end
-
